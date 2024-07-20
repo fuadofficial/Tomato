836e85d0e3ffb2187c28d0bc945881374673e23c
fuad <ahmadfuadoorakam@gmail.com>
2024-07-20 08:49:59 +0530

    completed 4th task

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,21 @@
 import { useState } from "react";
 import Title from "./components/Title";
-import './App.css'
+import "./App.css";
+import FormOne from "./components/FormOne";
+import FormTwo from "./components/FormTwo";
 
 const App = () => {
   const [toggle, setToggle] = useState(false);
 
   return (
     <div className="container">
-        <button onClick={() => setToggle(!toggle)} className="toggle-button">
-          {toggle ? "Hide Button" : "Show Button"}
-        </button>
+      <button onClick={() => setToggle(!toggle)} className="toggle-button">
+        {toggle ? "Hide Button" : "Show Button"}
+      </button>
       {toggle && <Title />}
-      <div className="button-Container">
+      <div className="form-Container">
+        <FormOne />
+        <FormTwo />
       </div>
     </div>
   );
