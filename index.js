a844d76394ae9da8ace2f23e676966076ad3169d
unknown <ahmadfuadoorakam@gmail.com>
2024-10-16 13:10:30 -0700

    added index.js page for text js

---
+++ index.js
@@ -0,0 +1,13 @@
+// const array = ["fuad", "aju", "saju", "kappu",3]
+// const arrayaa = "fuad oorakam"
+// const arraya = [1, 2, 3, 4, 5]
+
+// for (let x of array) {
+//     console.log(x);
+// }
+// // for (let x of arrayaa) {
+// //     console.log(x);
+// // }
+// // for (let x of arraya) {
+// //     console.log(x);
+// // }
