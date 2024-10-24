98b1c95fc2b15bbf831748e084dd69ed135d0a9f
unknown <ahmadfuadoorakam@gmail.com>
2024-10-24 08:43:31 -0700

    creating cart page functionalities

--- client/src/context/StoreContext.jsx
+++ client/src/context/StoreContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useEffect, useState } from "react";
+import { createContext, useState } from "react";
 import { food_list } from "../assets/assets";
 export const StoreContext = createContext(null);
 
@@ -17,17 +17,13 @@ const StoreContextProvider = (props) => {
     setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] - 1 }));
   };
 
-  useEffect(() => {
-    console.log(cartItems);
-  }, [cartItems]);
-
   const contextValue = {
     food_list,
     cartItems,
     setCartItems,
     addToCart,
     removeFromCart,
-  };
+  };  
 
   return (
     <StoreContext.Provider value={contextValue}>
--- client/src/pages/Cart/Cart.jsx
+++ client/src/pages/Cart/Cart.jsx
@@ -1,10 +1,36 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import './Cart.css'
+import { StoreContext } from '../../context/StoreContext'
 
 const Cart = () => {
+
+  const { cartItems, food_list, removeFromCart } = useContext(StoreContext)
+  console.log(food_list);
+  console.log(cartItems);
+  
   return (
-    <div>
-      
+    <div className='cart'>
+      <div className="cart-items">
+        <div className="cart-items-title">
+          <p>Items</p>
+          <p>Title</p>
+          <p>Price</p>
+          <p>Quantity</p>
+          <p>Total</p>
+          <p>Remove</p>
+        </div>
+        <br />
+        <hr />
+        {food_list.map((item, index) => {
+          if (cartItems[item._id] > 0) {
+            return (
+              <div key={index} className="cart-item-title cart-items-item" >
+                <p>{item.name}</p>
+              </div>
+            )
+          }
+        })}
+      </div>
     </div>
   )
 }
